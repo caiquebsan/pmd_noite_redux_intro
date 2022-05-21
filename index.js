53d11105584a452b6d1024ab513c087b56161465
Caique Santos <caiquebsan@gmail.com>
2022-05-20 21:43:35 -0300

    definicao dos reducers

--- index.js
+++ index.js
@@ -24,3 +24,32 @@ const solicitarCashback = (nome, valor) => {
         }
     })
 }
+
+const historicoDePedidosCashbackReducer = (historicoDePedidosCashback = [], acao) => {
+    if(acao.type === "SOLICITAR_CASHBACK"){
+        return [
+            ...historicoDePedidosCashback, acao.payload
+        ]
+    }
+
+    return historicoDePedidosCashback
+} 
+
+const manipularCaixaReducer = (valorEmCaixa = 0, acao) => {
+    if(acao.type === "CRIAR_CONTRATO"){
+        return valorEmCaixa + acao.payload.valor
+    } else if(acao.type === "SOLICITAR_CASHBACK"){
+        return valorEmCaixa - acao.payload.valor
+    }
+    return valorEmCaixa
+}
+
+const listaDeContratosReducer = (listaDeContratos = [], acao) => {
+    if(acao.type === "CRIAR_CONTRATO")
+        return [...listaDeContratos, acao.payload.nome]
+    if(acao.type === "CANCELAR_CONTRATO"){
+        var listaDeContratosAlterada = listaDeContratos.filter((e) => { e === acao.valor.nome?false:true })
+        return listaDeContratosAlterada
+    }
+    return listaDeContratos
+}
